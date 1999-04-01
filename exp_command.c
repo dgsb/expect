1a63fefbc7c9d2f6653a1b1baf373817f4f84a69
unknown <>
1999-04-01 16:10:31 +0000

    This commit was manufactured by cvs2svn to create branch 'expect-5-31-branch'.
    Sprout from expect 1998-10-14 22:53:26 UTC cvsadmin 'Import of Expect v. 5.28.1'
    Cherrypick from master 1999-04-01 16:10:30 UTC libes 'Martin Forssen <maf@crt.se> provided fix to allow configure':
        DbgMkfl.in
        HISTORY
        INSTALL
        Makefile.in
        NEWS
        aclocal.m4
        configure
        configure.in
        example/autoexpect
        example/ftp-inband
        example/rftp
        example/weather
        exp_command.c
        exp_main_exp.c
        exp_main_sub.c
        exp_pty.c
        exp_regexp.c
        expect.c
        expect.man
        pty_termios.c
        tcldbg.h

--- exp_command.c
+++ exp_command.c
@@ -1247,7 +1247,12 @@ parent_error:
 
 	if (0 > (slave = getptyslave(ttycopy,ttyinit,stty_init))) {
 		restore_error_fd
-		errorlog("open(slave pty): %s\r\n",Tcl_ErrnoMsg(errno));
+
+		if (exp_pty_error) {
+			errorlog("open(slave pty): %s\r\n",exp_pty_error);
+		} else {
+			errorlog("open(slave pty): %s\r\n",Tcl_ErrnoMsg(errno));
+		}
 		exit(-1);
 	}
 	/* sanity check */
@@ -2279,6 +2284,10 @@ char **argv;
 	}
 	if (aflag && !(openarg || filename)) {
 		usage_error
+	}
+	if (current_append && (openarg || filename)) {
+		exp_error(interp,"cannot start logging without first stopping logging");
+		return TCL_ERROR;
 	}
 
 	logfile = 0;
